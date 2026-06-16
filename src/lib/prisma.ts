9ddb8dc9cb8ee0c8826ec88420a39dc40b9bd3e5
twiskou <kheireddinedebz@gmail.com>
2026-06-16 01:04:46 +0100

    fix problem login

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,6 +5,10 @@ import { createClient } from "@libsql/client";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
+  if (!process.env.DATABASE_URL && process.env.TURSO_DATABASE_URL) {
+    process.env.DATABASE_URL = process.env.TURSO_DATABASE_URL;
+  }
+
   const libsql = createClient({
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
